f66e9385d7fb311918456373e6152c5e39dcd21f
Max Eder <52962844+maxeder@users.noreply.github.com>
2025-08-07 10:57:43 +0000

    update index and css

--- src/index.mjs
+++ src/index.mjs
@@ -1,91 +1,212 @@
-import "./styles.css";
-// import * as d3 from "d3";
-// import { drawBarChart, isWorstCaseSel } from "./barchart.mjs";
-// import { drawRadialChart } from "./radialchart.mjs";
-// import caseData from "./caseData.json";
+//import * as d3 from "d3";
+import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7/+esm";
 
-// const CONFIG = {
-//   CASES: 5,
-//   IMAGES: {
-//     PRE_IMG: "src/preForest.svg",
-//     POST_IMG: "src/postForest.svg",
-//   },
-//   TRANSITION_DELAY: 1300,
-//   SUCCESS_MESSAGE:
-//     "Great, you chose the most sustainable case. A tree has been added to your forest!",
-// };
+import caseDataS1 from "./data/data_scenario_1.json";
+import caseDataS2 from "./data/data_scenario_2.json";
+import caseDataS3 from "./data/data_scenario_3.json";
+import caseDataS4 from "./data/data_scenario_4.json";
+import caseDataS5 from "./data/data_scenario_5.json";
 
-// initialise case data
-// function newCaseData() {
-//   const data = [];
-//   for (let i = 1; i < CONFIG.CASES + 1; i++) {
-//     data.push({ case: i, value: Math.floor(Math.random() * 81) + 10 }); // range 10-90
-//   }
-//   return data;
-// }
-//const caseData = newCaseData();
+const minValue = 0;
+const maxValue = 100;
 
-// drawBarChart(caseData);
+const colorScale = d3.scaleSequential(
+  [minValue, maxValue],
+  d3.interpolateRdYlGn
+);
 
-import { initApp } from "./bar_tree.mjs";
-import dataset from "./data.json"; // or wherever your data comes from
+const ALL_DATASETS = [
+  caseDataS1,
+  caseDataS2,
+  caseDataS3,
+  caseDataS4,
+  caseDataS5,
+];
+const CONFIG = { SORT: true };
+const TOTAL_CASES = 5;
 
-initApp();
+let finalizedSelections = [];
 
-// // initialise hourly data
-// function newHourlyData() {
-//   const data = [];
-//   for (let i = 1; i < 25; i++) {
-//     data.push({ hour: i, value: Math.floor(Math.random() * 100) });
-//   }
-//   return data;
-// }
-// const hourlyData = newHourlyData();
-// drawRadialChart(hourlyData);
+function updateURL(selections) {
+  const url = new URL(window.location);
+  url.searchParams.set("selections", selections.join("-"));
+  history.replaceState(null, "", url);
+}
 
-// // update radial data to show new graph
-// export function updateRadial() {
-//   const hourlyData = newHourlyData();
-//   drawRadialChart(hourlyData);
-// }
+function getSelectionsFromURL() {
+  const params = new URLSearchParams(window.location.search);
+  const raw = params.get("selections");
+  return raw ? raw.split("-").map(Number) : [];
+}
 
-// send button functionality
-// const button = document.getElementById("sendButton");
+export async function initApp() {
+  // Layout setup
+  const layout = d3.select("body").append("div").attr("id", "layout");
 
-// button.onclick = function () {
-//   console.log(isWorstCaseSel());
-// };
+  layout.append("div").attr("id", "tree-grid");
+  layout.append("div").attr("id", "bar-interaction");
 
-// function drawOverlay() {
-//   const overlay = document.createElement("div");
-//   overlay.id = "overlay";
+  finalizedSelections = getSelectionsFromURL();
 
-//   const preImg = document.createElement("img");
-//   preImg.src = CONFIG.IMAGES.PRE_IMG;
-//   overlay.appendChild(preImg);
+  console.log(finalizedSelections);
 
-//   const postImg = document.createElement("img");
-//   postImg.src = CONFIG.IMAGES.POST_IMG;
-//   postImg.className = "hide";
-//   overlay.appendChild(postImg);
+  for (let i = 0; i < finalizedSelections.length; i++) {
+    drawFinalTreeOnly(ALL_DATASETS[i], finalizedSelections[i], i);
+  }
 
-//   const successText = document.createElement("p");
-//   successText.className = "hide";
-//   successText.innerHTML = CONFIG.SUCCESS_MESSAGE;
-//   overlay.appendChild(successText);
+  if (finalizedSelections.length < TOTAL_CASES) {
+    const step = finalizedSelections.length;
+    drawBarChartWithPreview(ALL_DATASETS[step], step);
+  }
+}
 
-//   //  transition
-//   setTimeout(() => {
-//     [postImg, successText].forEach((element) => {
-//       element.classList.remove("hide");
-//       element.classList.add("active");
-//     });
-//   }, CONFIG.TRANSITION_DELAY);
+function drawBarChartWithPreview(dataIn, step) {
+  const container = d3.select("#bar-interaction");
+  container.html(""); // Clear any previous step
 
-//   document.body.appendChild(overlay);
+  const data = CONFIG.SORT ? d3.sort([...dataIn], (d) => d.value) : [...dataIn];
+  const width = 400;
+  const height = 300;
+  const margin = { top: 20, right: 20, bottom: 30, left: 40 };
 
-//   // click anywhere to close overlay
-//   overlay.onclick = function () {
-//     overlay.remove();
-//   };
-// }
+  let selectedCase = data.reduce((a, b) => (a.value > b.value ? a : b)).case;
+
+  const svg = container
+    .append("svg")
+    .attr("width", width)
+    .attr("height", height);
+
+  const chartGroup = svg
+    .append("g")
+    .attr("transform", `translate(${width / 2}, ${height / 2})`);
+
+  const chartWidth = width - margin.left - margin.right;
+  const chartHeight = height - margin.top - margin.bottom;
+
+  const x = d3
+    .scaleBand()
+    .domain(data.map((d) => `Case ${d.case}`))
+    .range([-chartWidth / 2 + margin.left, chartWidth / 2 - margin.right])
+    .padding(0.1);
+
+  const y = d3
+    .scaleLinear()
+    .domain([0, 100])
+    .range([chartHeight / 2 - margin.bottom, -chartHeight / 2 + margin.top]);
+
+  chartGroup.append("g").attr("class", "bars");
+
+  chartGroup
+    .append("g")
+    .attr("transform", `translate(0,${y(0)})`)
+    .call(d3.axisBottom(x));
+
+  chartGroup
+    .append("g")
+    .attr("transform", `translate(${x.range()[0]},0)`)
+    .call(d3.axisLeft(y));
+
+  renderBars();
+
+  function renderBars() {
+    chartGroup.selectAll(".tooltip").remove();
+
+    chartGroup
+      .select(".bars")
+      .selectAll("rect")
+      .data(data)
+      .join("rect")
+      .attr("x", (d) => x(`Case ${d.case}`))
+      .attr("y", (d) => y(d.value))
+      .attr("width", x.bandwidth())
+      .attr("height", (d) => y(0) - y(d.value))
+      .attr("fill", (d) => colorScale(d.value))
+      .style("stroke", (d) => (d.case === selectedCase ? "#000" : "none"))
+      .style("stroke-width", "2px")
+      .on("click", (event, d) => {
+        selectedCase = d.case;
+        renderBars();
+        // updateSVG(data, selectedCase, step, colorScale, minValue, maxValue);
+        updateSVG(data, selectedCase, step);
+      });
+  }
+
+  const treeGrid = d3.select("#tree-grid");
+
+  const previewSvg = treeGrid
+    .append("svg")
+    .attr("id", `tree_svg_${step}`)
+    .attr("class", "tree_svg")
+    .attr("width", 200)
+    .attr("height", 200);
+
+  d3.xml("src/tree.svg").then((dataXml) => {
+    const importedNode = document.importNode(dataXml.documentElement, true);
+    previewSvg.node().appendChild(importedNode);
+
+    previewSvg.selectAll("*").each(function () {
+      this.removeAttribute("style");
+    });
+
+    // updateSVG(data, selectedCase, step, colorScale, minValue, maxValue);
+    updateSVG(data, selectedCase, step);
+  });
+
+  container
+    .append("button")
+    .text("Finalize selection")
+    .style("margin-top", "10px")
+    .on("click", () => {
+      finalizedSelections.push(selectedCase);
+      updateURL(finalizedSelections);
+      //drawFinalTreeOnly(data, selectedCase, step);
+      container.html(""); // clear bar interaction
+
+      if (finalizedSelections.length < TOTAL_CASES) {
+        drawBarChartWithPreview(
+          ALL_DATASETS[finalizedSelections.length],
+          finalizedSelections.length
+        );
+      }
+    });
+}
+
+function drawFinalTreeOnly(data, selectedCase, step) {
+  const container = d3.select("#tree-grid");
+
+  const svg = container
+    .append("svg")
+    .attr("id", `final_tree_${step}`)
+    .attr("class", "tree_svg");
+
+  d3.xml("src/tree.svg").then((dataXml) => {
+    const importedNode = document.importNode(dataXml.documentElement, true);
+    svg.node().appendChild(importedNode);
+
+    svg.selectAll("*").each(function () {
+      this.removeAttribute("style");
+    });
+
+    updateSVG(data, selectedCase, step);
+  });
+}
+
+function updateSVG(data, selectedCase, step) {
+  const selectedData = data.find((d) => d.case === selectedCase);
+  if (!selectedData) return;
+
+  const sizeScale = d3
+    .scaleLinear()
+    .domain([minValue, maxValue])
+    .range([100, 300]);
+
+  const svgSize = sizeScale(selectedData.value);
+
+  d3.select(`#tree_svg_${step}, #final_tree_${step}`)
+    .transition()
+    .duration(300)
+    .ease(d3.easeQuadOut)
+    .attr("width", svgSize)
+    .attr("height", svgSize)
+    .attr("fill", colorScale(selectedData.value));
+}
